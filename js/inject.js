06cb78c22ca2fbec2bc862886ddce20c86f7b5c2
安先生 <anaf@163.com>
2020-04-13 00:51:02 +0800

    init

---
+++ js/inject.js
@@ -0,0 +1,3 @@
+// window.postMessage({"test": '222222dadad'}, '*');
+
+$("#login-username").val('13123123');
---
+++ js/popup.js
@@ -0,0 +1,8 @@
+// console.log(window.location.host)
+// chrome.tabs.create({url: 'https://www.baidu.com'});
+
+// var host = window.location.host;
+
+// if(host == 'www.163.com'){
+	chrome.tabs.create({url: 'https://www.baidu.com'});
+// }
