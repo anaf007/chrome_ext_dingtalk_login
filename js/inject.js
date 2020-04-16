8dc61ae47a40c913cc20ed3fff3cd0cd6fd3eacc
anaf <anaf@163.com>
2020-04-16 15:26:04 +0800

    完成

--- js/inject.js
+++
@@ -1,6 +0,0 @@
-//这里才能dom操作   虽然content也能dom操作 但是无效
-
-
-$("button").click(function(){
-    $("button").html("88888888");
-});
--- js/popup.js
+++ js/popup.js
@@ -1,34 +1,31 @@
 
-
 // 获取当前选项卡ID
-// function getCurrentTabId(callback)
-// {
-// 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-// 	{
-// 		if(callback) callback(tabs.length ? tabs[0].id: null);
-// 	});
-// }
+function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
 
+// 向content-script主动发送消息
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
 
-
-
-$('#button').click(e => {
-	var bg = chrome.extension.getBackgroundPage();
-	bg.testBackground();
-});
-
-
-// var targetExtensionId = "ajikaapbncbojcjdckmdapjikohjbpbf"; // 插件的ID
-// 	chrome.runtime.sendMessage(targetExtensionId, {type: 'MsgFromPage', msg: 'Hello, I am page~'}, function(response) {
-// 	console.log(response);
-// });
 
 chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    // 可以针对sender做一些白名单检查
-    // sendResponse返回响应
+
     if (request.type == 'MsgFromPage') {
-	  sendResponse({tyep: 'MsgFromChrome', msg: 'Hello, I am chrome extension~'});
-	  var bg = chrome.extension.getBackgroundPage();
-	bg.testBackground();
+		sendMessageToContentScript({cmd:'52pojie', size: 42}, function(response){});
+		
     }
 });
+
